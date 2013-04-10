20bb80c53a5e1cf063b225a64dfc74972c5049dd
kubo0180 <gibson02122569@gmail.com>
2013-04-11 08:29:08 +0900

    Add a Cart button on store/index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   attr_accessible :description, :fee, :gmaps, :image_url, :latitude, :longitude, :title
   validates :description, :fee, :image_url, :title, presence: true
   validates :fee, numericality: {greater_than_or_equal_to: 0.01 }
@@ -6,4 +9,14 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i, 
     message: 'is not image file. Please set the url of image file.'
   }
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'There are some Line items.')
+      return false
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Uselesshop::Application.routes.draw do
+  resources :line_items
+
+
+  resources :carts
+
+
   get "stores/index"
 
   resources :products
