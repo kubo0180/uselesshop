af898322e770f71628766eb4af988b4b20afa81f
kubo0180 <gibson02122569@gmail.com>
2013-04-15 00:31:50 +0900

    Add a smart cart and Ajax post

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class CartsController < ApplicationController
   # GET /carts
   # GET /carts.json
@@ -13,11 +15,16 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
-    @cart = Cart.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @cart }
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "User accessed invaild cart #{params[:id]}"
+      redirect_to stores_url, notice: 'Invalid cart number'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @cart }
+      end
     end
   end
 
@@ -72,12 +79,13 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart = Cart.find(params[:id])
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url }
-      format.json { head :no_content }
+      format.html { redirect_to stores_url }
+      format.json { head :ok }
     end
   end
 end
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,6 @@
 class StoresController < ApplicationController
   def index
     @products = Product.order(:id)
+    @cart = current_cart
   end
 end
